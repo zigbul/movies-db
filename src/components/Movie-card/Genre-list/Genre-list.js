c62d8644be18a21ac2a27a12b6218bfe6eef29d9
Zigbul <parfenov.georgiy@bk.ru>
2021-03-31 12:11:21 +0300

    genres added

--- src/components/Movie-card/Genre-list/Genre-list.js
+++ src/components/Movie-card/Genre-list/Genre-list.js
@@ -4,16 +4,33 @@ import { MyContext } from '../../../context';
 const GenreList = ({ genre_ids }) => {
 
    const genres = useContext(MyContext);
-   console.log(genres);
+
+   let cache;
+   let ln1 = genre_ids.length, ln2 = genres.length;
+   const genreList = [];
+
+   for (let i = 0; i < ln2; ++i) {
+      cache = genres[i].id;
+
+      for (let j = 0; j < ln1; ++j) {
+         if (cache === genre_ids[j]) {
+            genreList.push(genres[i].name);
+            break;
+         }
+      }
+   }
+   
+   const elements = genreList.map( genreName => {
+      return (
+         <li className="genre-list__item">
+            <p className="genre-list__text">{genreName}</p>
+         </li>
+      )
+   })
 
    return (
       <ul className="film-card__genre genre-list">
-         <li className="genre-list__item">
-            <p className="genre-list__text">Action</p>
-         </li>
-         <li className="genre-list__item">
-            <p className="genre-list__text">Drama</p>
-         </li>
+         {elements}
       </ul>
    );
 };
